a5b3845bf75e06082e76c92863cd81cb04442751
Ivan <vanek975@gmail.com>
2024-09-26 12:28:30 +0300

    feat(Models): Added Failure models

---
+++ lib/onix_flutter_core_models.dart
@@ -0,0 +1,5 @@
+library onix_flutter_core_models;
+
+export 'src/domain/failure/failure.dart';
+export 'src/domain/failure/networking/api_failure.dart';
+export 'src/domain/failure/networking/canceled_request_failure.dart';
---
+++ lib/src/domain/failure/networking/canceled_request_failure.dart
@@ -0,0 +1,5 @@
+import 'package:onix_flutter_core_models/src/domain/failure/failure.dart';
+
+class CanceledRequestFailure implements Failure {
+  const CanceledRequestFailure();
+}
